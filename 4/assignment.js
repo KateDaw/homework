392bd7377b8adbd265b12af1b5102734ee20cea9
KateDaw <fffoooxx.2000@gmail.com>
2020-11-08 12:38:13 +0300

    fourth

---
+++ 4/assignment.js
@@ -0,0 +1,25 @@
+const randomNumber = Math.random();
+
+ if (randomNumber >= 0.7) {
+    alert(randomNumber);
+}; //first task
+
+  let array = [2, 4, 6, 10];
+
+ for (let i = 0; i <= array.length -1; i++){
+     console.log(array[i]);
+ }
+  let arr = [4, 6, 89, 3, 356];
+  let i = 4;
+   
+  while (i < arr.length) {
+   console.log(arr[i]);
+i--; 
+} // second/third task
+
+
+const randomNumber2 = Math.random();
+
+if (randomNumber2 > 0.7 && randomNumber > 0.7 || randomNumber2 < 0.2 || randomNumber < 0.2) {
+  alert('That is perfect')
+} //fourth task
