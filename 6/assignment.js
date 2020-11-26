84cdd9ffeb4f110dc31a4a569b7d1d58d60e35ce
KateDaw <fffoooxx.2000@gmail.com>
2020-11-26 18:57:34 +0300

    6

---
+++ 6/assignment.js
@@ -0,0 +1,19 @@
+//------------first task-------------
+let o = document.getElementById('task-1');
+o.style.background = 'black';
+o.style.color = 'white';
+//------------second task-------------
+let i = document.querySelector('title');
+console.log(i);
+document.querySelector("title").innerHTML = "Домашнее задание сделано!";
+//-------------third task----------------
+let u = document.querySelector('h1');
+console.log(u);
+document.querySelector("h1").innerHTML = "Домашнее задание сделано!";
+
+
+
+
+
+
+
